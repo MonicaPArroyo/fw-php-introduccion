ab0b0c02c58c749efe8fa7d22a0246cb7df0ae8b
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-02-17 18:04:05 -0600

    Add files via upload

---
+++ views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium quas quibusdam tenetur amet, ratione nam quisquam est eligendi hic facere! Ipsum, reiciendis voluptate magnam rem error sint praesentium excepturi amet?</p>
+</div>
